96f4d5435e66a738a318e8c6fc6976946ee1716b
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-13 15:36:36 +0000

    update

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 -- Script ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, fans as nb_fans FROM metal_bands ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
