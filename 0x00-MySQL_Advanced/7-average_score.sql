dd92a4f5f3f91d99167ee3ed71c5170891cea05a
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-14 03:03:05 +0000

    complete task 7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,11 @@
+-- Script creates a stored procedure ComputeAverageScoreForUser that computes and stores the average score for a student 
+DELIMITER &&
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+	BEGIN
+		DECLARE average FLOAT DEFAULT NULL;
+		SET average = (SELECT AVG(score) FROM corrections WHERE user_id=user_id);
+		UPDATE users SET average_score = average WHERE id=user_id;
+	END;
+&&
+
+DELIMITER ;
