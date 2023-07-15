4d980ec1e6bedf0b10d6a9b296e94cf169e88762
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-15 15:12:56 +0000

    update

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,11 +1,12 @@
--- Script creates a stored procedure ComputeAverageScoreForUser that computes and stores the average score for a student 
-DELIMITER &&
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
-	BEGIN
-		DECLARE average FLOAT DEFAULT NULL;
-		SET average = (SELECT AVG(score) FROM corrections WHERE user_id=user_id);
-		UPDATE users SET average_score = average WHERE id=user_id;
-	END;
-&&
-
+-- Script creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
+BEGIN
+	DECLARE num INT DEFAULT 0;
+	DECLARE sums INT DEFAULT 0;
+	SET num = (SELECT COUNT(score) FROM corrections WHERE corrections.user_id=user_id);
+	SET sums = (SELECT SUM(score) FROM corrections WHERE corrections.user_id=user_id);
+	UPDATE users SET average_score = sums/num WHERE id=user_id;
+END
+//
 DELIMITER ;
