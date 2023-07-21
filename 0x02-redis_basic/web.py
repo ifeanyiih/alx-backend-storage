9872b606d787e3b36795672548a1001e5ab450a2
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-21 23:40:21 +0000

    modify

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python3
-""" Implementing an expiring web cache and tracker
-    obtain the HTML content of a particular URL and returns it """
-import redis
+"""In this tasks, we will implement a get_page function
+(prototype: def get_page(url: str) -> str:). The core of the function
+is very simple. It uses the requests module to obtain the HTML content
+of a particular URL and returns it.
+
+Start in a new file named web.py and do not reuse the code written in
+exercise.py.
+
+Inside get_page track how many times a particular URL was accessed in the key
+"count:{url}" and cache the result with an expiration time of 10 seconds.
+
+Tip: Use http://slowwly.robertomurray.co.uk to simulate a slow response and
+test your caching.
+"""
+from redis import Redis
 import requests
-r = redis.Redis()
-count = 0
+from typing import Callable
+from functools import wraps
+
+redis = Redis()
+
+
+def call_count(method: Callable) -> Callable:
+    """Counts how many times a function is called"""
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """wraps the method"""
+        return method(*args, **kwargs)
+    return wrapper
 
 
 def get_page(url: str) -> str:
-    """ track how many times a particular URL was accessed in the key
-        "count:{url}"
-        and cache the result with an expiration time of 10 seconds """
-    r.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
-    return resp.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    """Obtains the HTML content of a particular URL
+    and returns it
+    Args:
+        url (str): a url string
+    Returns:
+        (str): the HTML content of the url
+    """
+    get = call_count(requests.get)
+    req = get(url)
+    key = f"count:{url}"
+    redis.incr(key, 1)
+    redis.expire(key, 10)
+    return req.text
