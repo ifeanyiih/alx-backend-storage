380edc9f868f3c70c4a74d6f25a0e4edd9aa5bd4
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-21 23:34:35 +0000

    modify

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,45 +1,22 @@
 #!/usr/bin/env python3
-"""In this tasks, we will implement a get_page function
-(prototype: def get_page(url: str) -> str:). The core of the function
-is very simple. It uses the requests module to obtain the HTML content
-of a particular URL and returns it.
-
-Start in a new file named web.py and do not reuse the code written in
-exercise.py.
-
-Inside get_page track how many times a particular URL was accessed in the key
-"count:{url}" and cache the result with an expiration time of 10 seconds.
-
-Tip: Use http://slowwly.robertomurray.co.uk to simulate a slow response and
-test your caching.
-"""
+""" Implementing an expiring web cache and tracker
+    obtain the HTML content of a particular URL and returns it """
 import redis
 import requests
-from typing import Callable
-from functools import wraps
-
-
-def call_count(method: Callable) -> Callable:
-    """Counts how many times a function is called"""
-    @wraps(method)
-    def wrapper(*args, **kwargs):
-        """wraps the method"""
-        redis_ = redis.Redis()
-        key = f"{args[0]}"
-        redis_.incr(key, 1)
-        redis_.expire(key, 10)
-        return method(*args, **kwargs)
-    return wrapper
+r = redis.Redis()
+count = 0
 
 
 def get_page(url: str) -> str:
-    """Obtains the HTML content of a particular URL
-    and returns it
-    Args:
-        url (str): a url string
-    Returns:
-        (str): the HTML content of the url
-    """
-    get = call_count(requests.get)
-    req = get(url)
-    return req.text
+    """ track how many times a particular URL was accessed in the key
+        "count:{url}"
+        and cache the result with an expiration time of 10 seconds """
+    r.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    return resp.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
